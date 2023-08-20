9000dd9cd608492a66408a35b37762928af230f4
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-08-20 06:36:48 -0400

    mobile sidenav fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@
  * Created Date: Sunday August 13th 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Sun August 20th 2023 1:29:44 
+ * Last Modified: Sun August 20th 2023 6:34:10 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
@@ -12,80 +12,57 @@
 import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
 import { MatDrawer } from "@angular/material/sidenav";
 
-/**
- * AppComponent
- * @classdesc - App component implementation
- */
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  /**
-   * Properties
-   */
   title = "twd";
   @ViewChild("drawer") drawer!: MatDrawer;
   isDesktop: boolean = true;
-  
 
   get isDrawerOpened(): boolean {
     return this.drawer ? this.drawer.opened : this.isDesktop;
   }
+  
   set isDrawerOpened(value: boolean) {
     if (this.drawer) {
       this.drawer.opened = value;
     }
   }
 
+  get contentClass(): string {
+    return this.isDesktop || this.isDrawerOpened ? '' : 'mobile-nav-active';
+  }
+  
+  get isMobileNavOpened(): boolean {
+    return !this.isDesktop && this.isDrawerOpened;
+  }
+  
   constructor() {}
 
-  /**
-   * ngOnInit
-   * @method - Life cycle hook
-   */
   ngOnInit() {
     this.isDesktop = window.innerWidth >= 1024;
     window.addEventListener("resize", this.onResize);
   }
 
-  /**
-   * ngOnDestroy
-   * @method - Life cycle hook
-   */
   ngOnDestroy() {
     window.removeEventListener("resize", this.onResize);
   }
 
-  /**
-   * toggleDrawer
-   * @method - Toggle drawer
-   * @returns {void}
-   */
   toggleDrawer(): void {
     this.isDrawerOpened = !this.isDrawerOpened;
   }
 
-  /**
-   * closeMobile
-   * @method - Close drawer in mobile mode
-   * @returns {void}
-   */
   closeMobile(): void {
     if (!this.isDesktop) {
       this.isDrawerOpened = false;
     }
   }
 
-  /**
-   * onResize
-   * @method - On resize
-   * @returns {void}
-   */
   onResize = (): void => {
     this.isDesktop = window.innerWidth >= 1024;
     this.isDrawerOpened = this.isDesktop;
   };
-
 }
