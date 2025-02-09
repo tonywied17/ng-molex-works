b21c76124ed665d12a086a3781ec788ae56245f1
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2025-02-08 21:40:11 -0500

    Icons, and share image for open graph tags

--- src/app/_components/about/contact/quote-request/quote-request.component.ts
+++ src/app/_components/about/contact/quote-request/quote-request.component.ts
@@ -4,7 +4,7 @@
  * Created Date: Monday August 14th 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Sat February 8th 2025 8:38:41 
+ * Last Modified: Sat February 8th 2025 9:39:39 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
